5e55fa2fd40d9b4b1fbfae4715dce8a4da3a79ea
Raice Hannay <raice.hannay@xero.com>
2020-03-04 16:11:08 +1300

    fix prettier conflict with jsx-wrap-multilines

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = {
   extends: ["airbnb-typescript", require.resolve("./base.js")],
   rules: {
-    "react/state-in-constructor": "off",
-    "react/static-property-placement": ["error", "static public field"],
     "react/destructuring-assignment": "off",
+    "react/jsx-wrap-multilines": "off",
     "react/jsx-one-expression-per-line": "off",
     "react/jsx-props-no-spreading": "off",
     "react/prop-types": "off",
-    "react/sort-comp": "off"
+    "react/sort-comp": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": ["error", "static public field"]
   }
 };
